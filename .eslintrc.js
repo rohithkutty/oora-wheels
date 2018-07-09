2bb5488cac192e06e084cef855afebd519693ac8
Rohith Surya <rohith_gonela@yahoo.com>
2018-07-09 11:47:31 +0530

    Initial wireframe with eslint configured

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "rules": {
+      "strict": 0
+    },
+    "env": {
+      "browser": true,
+      "es6": true,
+      "jquery": true
+    },
+    "parserOptions": {
+      "sourceType": "module"
+    },
+    "plugins": [
+      "react"
+    ],
+    "rules": {
+      "comma-dangle": [
+        "warn",
+        "never"
+      ],
+      "indent": [
+        "warn",
+        4
+      ],
+      "linebreak-style": [
+        "warn",
+        "unix"
+      ],
+      "quotes": [
+        "error",
+        "single"
+      ],
+      "semi": [
+        "error",
+        "always"
+      ],
+      /* Advanced Rules*/
+      "no-unused-expressions": "warn",
+      "no-useless-concat": "warn",
+      "block-scoped-var": "error",
+      "consistent-return": "error"
+    }
+  };
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello
+      </div>
+    );
+  }
+}
+
+export default App;
